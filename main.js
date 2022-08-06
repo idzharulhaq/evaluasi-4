7a5ef730cf1a7d8b1acc3d3b89f29f009bf4f4f5
idzharulhaq <ihaq1013@gmail.com>
2022-08-06 10:46:35 +0700

    evaluasi 4

---
+++ main.js
@@ -0,0 +1,53 @@
+// nama : izharul haq/arull
+// soal 1
+// perbedaan operator a.(==) dan b.(===)
+// a.
+5 == 5
+// ket:hasilny adalah true dikarnakan nilai nya sama 
+"5" == 5
+// ket:hasilnya adaah false dikarnkan nilai nya ada yang menggunakan string
+// b.
+5 === 5
+// ket:hasilny adalah true dikarnakan nilai nya sama 
+"5" === 5
+// ket:ket:hasilny adalah true dikarnakan dia akan dikerjakan dua kali
+
+// soal 2
+// operator ternary
+var a = 6 ?
+    console.log(7) : (8)
+
+// 3.
+let datafilm = [
+    {
+        id: 1,
+        title: 'tragedi atap jebol',
+        genre: ["action", "romance", "comedy", "horor"],
+        actor: ["indra", "ipul", "david", "moezha", "arull"],
+        ratting: 9.05,
+        synopsis: 'lorem ipsum dolor sit amet , cosectetur adipiscing elit',
+        type:'movie', // movie // tv
+        producer: ["pit movic", "indra music live"],
+        studio: 'pesantren it',
+        status: true // true => sudah rilis // false => belum rilis
+    }
+]
+
+//aku sangat bingung sekali 
+// pesantren it = bisa gak bisa harus bisa insya Allah
+
+var datafilm1 = [
+    {
+        id : 2,
+        title: 'start up',
+        genre: ["romance","comedy"],
+        actor: ["seo dal-mi","seo zuu-mi","park jung"],
+        ratting: 8.07,
+        synopsis: 'kwkwkwkwkwkwkwkwkwkww lucu bgt',
+        type: 'drakor',
+        producer: ["netflix"],
+        studio: 'my world',
+        status: true // true => sudah rilis // false => belum rilis
+    }
+]
+
